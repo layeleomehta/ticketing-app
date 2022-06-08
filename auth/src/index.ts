79b94547361a98fc1707bbcb44dd741462a7aa96
Laye Mehta <layeamehta@gmail.com>
2022-06-07 20:01:53 -0600

    configured ingress-nginx to serve as ingress controller, tested and it works

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,6 +4,10 @@ import {json} from "body-parser";
 const app = express(); 
 app.use(json()); 
 
+app.get('/api/users/currentuser', (req, res) => {
+    res.send("Yoyoooo wagwaaan!!!!!"); 
+})
+
 app.listen(3000, ()=> {
     console.log("server listening on port 3000"); 
 })
