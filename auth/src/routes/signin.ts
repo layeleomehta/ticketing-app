fefb7901648d0699be48ee41e8eb55477466fe65
Laye Mehta <layeamehta@gmail.com>
2022-06-08 13:43:22 -0600

    added validation for email and password in req body for auth service

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,26 @@
 import express, {Request, Response} from 'express'; 
+import { body, validationResult } from 'express-validator';
 
 const router = express.Router(); 
 
-router.post('/api/users/signin', (req: Request, res: Response) => {
-    res.send("Signin"); 
+router.post('/api/users/signin', 
+            [ body('email')
+                .isEmail()
+                .withMessage("Please enter a valid email!"), 
+              body('password')
+                .trim()
+                .isLength({ min: 4, max: 20 })
+                .withMessage("Please enter a password!")
+            ],
+            (req: Request, res: Response) => {
+                const errors = validationResult(req); 
+
+                if(!errors.isEmpty()){
+                    return res.send(errors.array()); 
+                }
+
+                const { email, password } = req.body; 
+                res.send("Hi"); 
 })
 
 export { router as signinRouter }; 
