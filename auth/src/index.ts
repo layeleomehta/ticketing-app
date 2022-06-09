2321825bce2884a876bfe82c6cc595dd459002fd
Laye Mehta <layeamehta@gmail.com>
2022-06-09 11:42:24 -0600

    connected to mongodb pod using cluster ip service, saving users in db currently without hashing password

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,6 +5,7 @@ import { signinRouter } from "./routes/signin";
 import { signupRouter } from "./routes/signup";
 import { signoutRouter } from "./routes/signout";
 import { errorHandler } from "./middlewares/error-handler";
+import mongoose from "mongoose";
 
 const app = express(); 
 app.use(json()); 
@@ -16,6 +17,17 @@ app.use(currentUserRouter);
 
 app.use(errorHandler); 
 
+const start = async () => {
+    try {
+        await mongoose.connect("mongodb://auth-mongo-srv:27017/auth"); 
+        console.log("connected to db"); 
+    } catch (err) {
+        console.error(err); 
+    }
+}
+
 app.listen(3000, ()=> {
     console.log("server listening on port 3000"); 
 })
+
+start(); 
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,9 +1,48 @@
-import express, {Request, Response} from 'express'; 
+import express, {NextFunction, Request, Response} from 'express'; 
+import { body, validationResult } from 'express-validator';
+import { RequestValidationError } from '../errors/request-validation-error';
+import { User } from '../models/user';
+
 
 const router = express.Router(); 
 
-router.post('/api/users/signup', (req: Request, res: Response) => {
-    res.send("Signup"); 
+router.post('/api/users/signup',
+    [ 
+    body('email')
+      .isEmail()
+      .withMessage("Please enter a valid email!"), 
+    body('password')
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage("Please enter a password!")
+    ],
+    async (req: Request, res: Response, next: NextFunction) => {
+    const errors = validationResult(req); 
+
+    if(!errors.isEmpty()){
+        next(new RequestValidationError(errors.array())); 
+    }
+
+    const { email, password } = req.body; 
+
+    // check if user already exists
+    const existingUser = await User.findOne({ email }); 
+    if(existingUser){
+        console.log("Email is in use"); 
+        return res.send({}); 
+    }
+
+    // hash password
+
+    // create new user and save in mongodb
+    const user = User.build({ email, password }); 
+    await user.save(); 
+
+    // send back token to user
+
+
+    res.status(201).send(user); 
+
 })
 
 export { router as signupRouter }; 
