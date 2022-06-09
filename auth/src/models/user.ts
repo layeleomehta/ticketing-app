accc788744f17342aca78627cc82e85a8c85c221
Laye Mehta <layeamehta@gmail.com>
2022-06-09 10:21:26 -0600

    added input, model and document interfaces for ts typechecking with mongoose

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,9 +1,22 @@
 import mongoose from "mongoose";
 
+// interface for type checking an entry into User model
 interface UserAttrs {
     email: string, 
     password: string
 }
+
+// interface which describes properties of a User model
+interface UserModel extends mongoose.Model<UserDoc> {
+    build(attrs: UserAttrs): UserDoc; 
+}
+
+// interface which describes properties of User document (document is instance of model)
+interface UserDoc extends mongoose.Document {
+    email: string, 
+    password: string
+}
+
 
 const userSchema = new mongoose.Schema({
     email: {
@@ -16,11 +29,11 @@ const userSchema = new mongoose.Schema({
     }
 }); 
 
-const User = mongoose.model('User', userSchema);
-
-const buildUser = (attr: UserAttrs) => {
+userSchema.statics.build = (attr: UserAttrs) => {
     const newUser = new User(attr); 
     return newUser; 
 }
+
+const User = mongoose.model<UserDoc, UserModel>('User', userSchema);
 
 export { User }; 
