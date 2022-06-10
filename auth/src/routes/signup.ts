5e1aa7d2f57f8f11beb21f067afda8ec9adfb3c5
Laye Mehta <layeamehta@gmail.com>
2022-06-09 20:33:41 -0600

    created jwt and set a cookie with user info upon successful signup

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,7 +1,10 @@
 import express, {NextFunction, Request, Response} from 'express'; 
 import { body, validationResult } from 'express-validator';
+import jwt from 'jsonwebtoken';
 import { RequestValidationError } from '../errors/request-validation-error';
 import { User } from '../models/user';
+import { Password } from '../services/password';
+
 
 
 const router = express.Router(); 
@@ -31,15 +34,21 @@ router.post('/api/users/signup',
         console.log("Email is in use"); 
         return res.send({}); 
     }
+    
 
-    // hash password
-
-    // create new user and save in mongodb
+    // create new user and save in mongodb, password is hashed in mongo pre 'save' hook in user.ts
     const user = User.build({ email, password }); 
     await user.save(); 
 
-    // send back token to user
+    // create JWT and send back to user as cookie
+    const jwtToken = jwt.sign({
+        id: user.id, 
+        email: user.email
+    }, 'secret'); 
 
+    req.session = {
+        jwt: jwtToken
+    }; 
 
     res.status(201).send(user); 
 
