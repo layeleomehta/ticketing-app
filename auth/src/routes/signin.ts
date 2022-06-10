86e86baee823229940e2e51f5cbd2615c694dbf4
Laye Mehta <layeamehta@gmail.com>
2022-06-10 11:48:45 -0600

    created BadRequestError custom error

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -2,22 +2,31 @@ import express, {Request, Response} from 'express';
 import { body } from 'express-validator';
 import { DatabaseConnectionError } from '../errors/database-connection-error';
 import { validateRequest } from '../middlewares/validate-request';
+import { User } from '../models/user';
 
 const router = express.Router(); 
 
 router.post('/api/users/signin', 
-            [ body('email')
-                .isEmail()
-                .withMessage("Please enter a valid email!"), 
-              body('password')
-                .trim()
-                .notEmpty()
-                .withMessage("Please enter a password!")
-            ], 
-            validateRequest, 
-            (req: Request, res: Response) => {
-                const { email, password } = req.body; 
-                res.send("Hi"); 
+    [ body('email')
+        .isEmail()
+        .withMessage("Please enter a valid email!"), 
+        body('password')
+        .trim()
+        .notEmpty()
+        .withMessage("Please enter a password!")
+    ], 
+    validateRequest, 
+    (req: Request, res: Response) => {
+        const { email, password } = req.body; 
+
+        // check if user with the email exists, if not throw error
+        const existingUser = await User.findOne({ email }); 
+        if(!existingUser){
+        }
+
+        // compare hashed password with stored user password, throw error if passwords don't match
+
+        // send back token
 })
 
 export { router as signinRouter }; 
