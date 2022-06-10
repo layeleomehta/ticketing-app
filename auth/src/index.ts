15b2f84c6b082a8ad9dc7354bfe82d6ab9e06165
Laye Mehta <layeamehta@gmail.com>
2022-06-10 16:39:49 -0600

    split index.ts into app.ts and index.ts to isolate express app object... to be used with supertest

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,27 +1,5 @@
-import express from "express"; 
-import { json } from "body-parser"; 
-import { currentUserRouter } from "./routes/current-user";
-import { signinRouter } from "./routes/signin";
-import { signupRouter } from "./routes/signup";
-import { signoutRouter } from "./routes/signout";
-import { errorHandler } from "./middlewares/error-handler";
+import { app } from "./app"; 
 import mongoose from "mongoose";
-import cookieSession from "cookie-session";
-
-const app = express(); 
-app.set('trust proxy', true); 
-app.use(json()); 
-app.use(cookieSession({
-    signed: false, 
-    secure: true
-})); 
-
-app.use(signupRouter); 
-app.use(signinRouter); 
-app.use(signoutRouter); 
-app.use(currentUserRouter); 
-
-app.use(errorHandler); 
 
 const start = async () => {
     if(!process.env.JWT_KEY){
