a01f0a40f2f2b138b360e68024afc3af69eca160
Laye Mehta <layeamehta@gmail.com>
2022-06-14 17:44:50 -0600

    set up signup page and boilerplate signup form

---
+++ client/pages/auth/signup.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+
+const signup = () => {
+    const [email, setEmail] = useState(""); 
+    const [password, setPassword] = useState(""); 
+
+    const onSubmit = (e) => {
+        e.preventDefault(); 
+        console.log("email: ", email); 
+        console.log("password: ", password); 
+    }
+
+  return (
+    <form onSubmit={onSubmit}>
+    <h1>Signup</h1>
+    <div className="mb-3">
+        <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+        <input type="email" value={email} onChange={e => setEmail(e.target.value)} className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+    </div>
+    <div className="mb-3">
+        <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+        <input type="password" value={password} onChange={e => setPassword(e.target.value)} className="form-control" id="exampleInputPassword1"/>
+    </div>
+    <button type="submit" className="btn btn-primary">Submit</button>
+    </form>
+  )
+}
+
+export default signup
