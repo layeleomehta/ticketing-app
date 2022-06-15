19b20f5665a8f06eb3a8380bcf67d3d678f37a4a
Laye Mehta <layeamehta@gmail.com>
2022-06-15 16:15:54 -0600

    conditionally rendering landing page based on signed in status of user

--- client/pages/index.js
+++ client/pages/index.js
@@ -3,18 +3,19 @@ import axios from "axios";
 import buildClient from '../api/build-client';
 
 const LandingPage = ({ currentUser }) => {
-  console.log(currentUser); 
-  return (
-    <div><h1>Landing page</h1></div>
+  console.log(currentUser)
+  return currentUser ? (
+    <h1>You are signed in!</h1>
+  ) : (
+    <h1>You are not signed in!</h1>
   )
 }
 
 LandingPage.getInitialProps = async (context) => { 
   const client = buildClient(context)
   const { data } = await client.get('/api/users/currentuser'); 
-  return {
-    currentUser: data
-  }; 
+
+  return data; 
 }
 
 export default LandingPage; 
