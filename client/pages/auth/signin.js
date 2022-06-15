2a04382379d6104ec0c1190d6cf31d624ce93f35
Laye Mehta <layeamehta@gmail.com>
2022-06-15 16:22:52 -0600

    created signin form

---
+++ client/pages/auth/signin.js
@@ -0,0 +1,37 @@
+import Router from "next/router"; 
+import React, { useState } from 'react'; 
+import useRequest from '../../hooks/use-request';
+
+const signin = () => {
+    const [email, setEmail] = useState(""); 
+    const [password, setPassword] = useState(""); 
+    const { doRequest, errors } = useRequest({ 
+        url: '/api/users/signin', 
+        method: 'post', 
+        body: { email, password }, 
+        onSuccess: () => Router.push('/')
+    }); 
+
+    const onSubmit = async (e) => {
+        e.preventDefault(); 
+        await doRequest(); 
+    }
+
+  return (
+    <form onSubmit={onSubmit}>
+    <h1>Sign In</h1>
+    <div className="mb-3">
+        <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+        <input type="email" value={email} onChange={e => setEmail(e.target.value)} className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+    </div>
+    <div className="mb-3">
+        <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+        <input type="password" value={password} onChange={e => setPassword(e.target.value)} className="form-control" id="exampleInputPassword1"/>
+    </div>
+    {errors}
+    <button type="submit" className="btn btn-primary">Sign In</button>
+    </form>
+  )
+}
+
+export default signin;
