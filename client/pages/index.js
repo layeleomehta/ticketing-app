a62b6f37aca8934493a3a63e9d2171dc39def31c
Laye Mehta <layeamehta@gmail.com>
2022-06-15 16:04:40 -0600

    created buildClient to decide between server or client requesting with axios, able to reach host on both server and client now

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,29 +1,20 @@
 import React from 'react'
 import axios from "axios"; 
+import buildClient from '../api/build-client';
 
-const LandingPage = () => {
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser); 
   return (
     <div><h1>Landing page</h1></div>
   )
 }
 
-LandingPage.getInitialProps = async () => { 
-  if(typeof window === 'undefined'){
-    // we are on server
-    const { data } = await axios.get("http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser", {
-      headers: {
-        Host: 'ticketing.dev'
-      }
-    }); 
-    console.log(data); 
-    return data; 
-
-  } else {
-    // we are on browser
-    const { data } = await axios.get("/api/users/currentuser"); 
-    console.log("browser")
-    return data; 
-  }
+LandingPage.getInitialProps = async (context) => { 
+  const client = buildClient(context)
+  const { data } = await client.get('/api/users/currentuser'); 
+  return {
+    currentUser: data
+  }; 
 }
 
 export default LandingPage; 
