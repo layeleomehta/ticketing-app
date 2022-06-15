3451a1ad9f681830740dff99537bd28bc9d3fd5c
Laye Mehta <layeamehta@gmail.com>
2022-06-14 19:38:15 -0600

    displaying signup errors to the client now

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -4,16 +4,21 @@ import React, { useState } from 'react';
 const signup = () => {
     const [email, setEmail] = useState(""); 
     const [password, setPassword] = useState(""); 
+    const [errors, setErrors] = useState([]); 
 
     const onSubmit = async (e) => {
         e.preventDefault(); 
-
-        const response = await axios.post("/api/users/signup", {
-            email, 
-            password
-        }); 
-
-        console.log(response); 
+        try {
+            const response = await axios.post("/api/users/signup", {
+                email, 
+                password
+            }); 
+    
+            console.log(response.data); 
+            
+        } catch (err) {
+            setErrors(err.response.data.errors); 
+        }
     }
 
   return (
@@ -27,6 +32,14 @@ const signup = () => {
         <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
         <input type="password" value={password} onChange={e => setPassword(e.target.value)} className="form-control" id="exampleInputPassword1"/>
     </div>
+        {errors.length > 0 && (
+            <div className='alert alert-danger'>
+                <h4>Oops... something went wrong:</h4>
+                <ul className='my-0'>
+                    {errors.map(error => <li key={error.message}>{error.message}</li>)}
+                </ul>
+            </div>
+        )}
     <button type="submit" className="btn btn-primary">Submit</button>
     </form>
   )
