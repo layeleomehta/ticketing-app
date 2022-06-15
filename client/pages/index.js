b8748e7686440058369cf1e291dc8f106dd5ce5d
Laye Mehta <layeamehta@gmail.com>
2022-06-15 15:07:07 -0600

    axios giving error when trying to use getInitialProps to make request to load balancer

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,9 +1,29 @@
 import React from 'react'
+import axios from "axios"; 
 
-const index = () => {
+const LandingPage = () => {
   return (
     <div><h1>Landing page</h1></div>
   )
 }
 
-export default index
+LandingPage.getInitialProps = async () => { 
+  if(typeof window === 'undefined'){
+    // we are on server
+    const { data } = await axios.get("http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser", {
+      Host: 'ticketing.dev'
+    }); 
+    console.log(data); 
+    return data; 
+
+  } else {
+    // we are on browser
+    const { data } = await axios.get("/api/users/currentuser"); 
+    console.log("browser")
+    return data; 
+  }
+  console.log("server")
+  return {}; 
+}
+
+export default LandingPage; 
