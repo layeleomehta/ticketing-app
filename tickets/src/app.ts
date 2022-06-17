40e7bc692650c2d178c44d4e7f0d1ce391257180
Laye Mehta <layeamehta@gmail.com>
2022-06-16 18:42:18 -0600

    created boilerplate for tickets service

---
+++ tickets/src/app.ts
@@ -0,0 +1,25 @@
+import express from "express"; 
+import { json } from "body-parser"; 
+import { currentUserRouter } from "./routes/current-user";
+import { signinRouter } from "./routes/signin";
+import { signupRouter } from "./routes/signup";
+import { signoutRouter } from "./routes/signout";
+import { errorHandler } from "@lm-tickets-microservices/common";
+import cookieSession from "cookie-session";
+
+const app = express(); 
+app.set('trust proxy', true); 
+app.use(json()); 
+app.use(cookieSession({
+    signed: false, 
+    secure: process.env.NODE_ENV !== 'test'
+})); 
+
+app.use(signupRouter); 
+app.use(signinRouter); 
+app.use(signoutRouter); 
+app.use(currentUserRouter); 
+
+app.use(errorHandler); 
+
+export { app }; 
