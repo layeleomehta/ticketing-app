39e5d18114590cce55a34fb2b269957118b9acc2
Laye Mehta <layeamehta@gmail.com>
2022-06-23 18:56:40 -0600

    added connection closing handlers

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -9,6 +9,11 @@ const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
 
 stan.on('connect', () => {
     console.log("Listener connected to NATS"); 
+
+    stan.on('close', () => {
+        console.log('NATS connection closed!');
+        process.exit();
+      });
     
     const options = stan.subscriptionOptions().setManualAckMode(true); 
     const subscription = stan.subscribe(
@@ -25,5 +30,8 @@ stan.on('connect', () => {
 
         msg.ack(); 
     }); 
+
+process.on('SIGINT', () => stan.close());
+process.on('SIGTERM', () => stan.close());
 
 }); 
