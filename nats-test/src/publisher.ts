537b94c14ac1f80dde0ecaa5129a2430ebd59849
Laye Mehta <layeamehta@gmail.com>
2022-07-01 14:26:52 -0600

    promisified the publish method in base publisher abstract class

--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -7,16 +7,20 @@ const stan: any = nats.connect('ticketing', 'abc', {
     url: "http://localhost:4222"
 }); 
 
-stan.on('connect', () => {
+stan.on('connect', async () => {
     console.log("Publisher connected to NATS"); 
 
-    const publisher = new TicketCreatedPublisher(stan); 
-    const data = {
-        id: '123', 
-        title: 'example-ticket', 
-        price: 30
-    }; 
-    publisher.publish(data); 
+    try {
+        const publisher = new TicketCreatedPublisher(stan); 
+        const data = {
+            id: '123', 
+            title: 'example-ticket', 
+            price: 30
+        }; 
+        await publisher.publish(data); 
+    } catch (err) {
+        console.error(err); 
+    }
 
     
 }); 
