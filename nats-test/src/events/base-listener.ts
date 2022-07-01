281df620a501b9ca1638ec5d088bb80798e104fc
Laye Mehta <layeamehta@gmail.com>
2022-06-30 22:32:22 -0600

    created base publisher class

--- nats-test/src/events/base-listener.ts
+++ nats-test/src/events/base-listener.ts
@@ -2,8 +2,8 @@ import {Stan, Message} from "node-nats-streaming";
 import { Subjects } from "./subjects";
 
 interface Event {
-    subject: Subjects, 
-    data: any
+    subject: Subjects;  
+    data: any; 
 }
 
 export abstract class Listener<T extends Event> {
