808e6ab4f492cd16dcd0b0543c04c5564cd4aa68
Laye Mehta <layeamehta@gmail.com>
2022-07-01 19:27:56 -0600

    added logic to gracefully exit nats client process if NATS pod fails

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -13,6 +13,12 @@ const start = async () => {
 
     try {
         await natsWrapper.connect('ticketing', 'abc', 'http://nats-srv:4222'); 
+        natsWrapper.client.on('close', () => {
+            console.log('The NATS connection is now closed!');
+            process.exit();
+          });
+          process.on('SIGINT', () => natsWrapper.client.close());
+          process.on('SIGTERM', () => natsWrapper.client.close());
         await mongoose.connect(process.env.MONGO_URI); 
         console.log("connected to db"); 
     } catch (err) {
