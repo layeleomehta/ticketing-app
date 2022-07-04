6cb2228db7ebb03acca3ea3f3b14d531b55f1c5d
Laye Mehta <layeamehta@gmail.com>
2022-07-04 13:25:34 -0600

    validation for the new order route

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,8 +1,22 @@
+import { validateRequest, requireAuth } from '@lm-tickets-microservices/common';
 import express, { Request, Response } from 'express'; 
+import { body } from 'express-validator';
+import mongoose from 'mongoose';
+
 
 const router = express.Router(); 
 
-router.post('/api/orders', (req: Request, res: Response) => {
+router.post('/api/orders', 
+    requireAuth, 
+    [
+        body('ticketId')
+            .not()
+            .isEmpty()
+            .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+            .withMessage('Ticket ID must be provided in field "ticketId"')
+    ],
+    validateRequest, 
+(req: Request, res: Response) => {
 
 }); 
 
